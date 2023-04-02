0c7b85da1d358261b3f2279324794ea7ab46e210
DrBrother <zhenya-2100@mail.ru>
2023-04-02 20:24:50 +0300

    - add mapping variables from json

--- scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/dto/response/StackOverflowResponse.java
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/dto/response/StackOverflowResponse.java
@@ -1,9 +1,13 @@
 package ru.tinkoff.edu.java.scrapper.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.time.OffsetDateTime;
 
 public record StackOverflowResponse(Item[] items) {
-    private record Item(OffsetDateTime creation_date, OffsetDateTime last_edit_date, boolean is_answered,
-                        Long question_id) {
+    private record Item(@JsonProperty(value = "creation_date") OffsetDateTime creationTime,
+                        @JsonProperty(value = "last_edit_date") OffsetDateTime lastEditTime,
+                        @JsonProperty(value = "is_answered") boolean isAnswered,
+                        @JsonProperty(value = "question_id") Long questionId) {
     }
 }
