de978fdf6033c20cc85399a3485556fdcd4ba4d3
DrBrother <zhenya-2100@mail.ru>
2023-04-16 15:11:37 +0300

    hw4; refactoring

--- migrations/scripts/02_create_link_table.sql
+++ migrations/scripts/02_create_link_table.sql
@@ -6,6 +6,6 @@ CREATE SEQUENCE IF NOT EXISTS link_id_seq;
 --changeset eshadrin:create_link_table
 CREATE TABLE IF NOT EXISTS link
 (
-    id  BIGINT PRIMARY KEY DEFAULT nextval('link_id_seq'),
-    url TEXT NOT NULL
+    id BIGINT DEFAULT nextval('link_id_seq') PRIMARY KEY,
+    url TEXT NOT NULL UNIQUE
 );
